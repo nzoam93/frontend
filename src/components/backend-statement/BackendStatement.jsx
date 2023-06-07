ee45ceaa5803523e5bb64a9fd72435265a433b3d
nzoam93 <nzoam93@gmail.com>
2023-06-07 13:24:27 -0700

    changed useState

--- src/components/backend-statement/BackendStatement.jsx
+++ src/components/backend-statement/BackendStatement.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import "./BackendStatement.css";
 
 const BackendStatement = () => {
-    const [statement, setStatement] = useState('base');
+    const [statement, setStatement] = useState('');
 
     useEffect(() => {
         fetch('/api/backend-statement')
             .then(response => response.json())
             .then(data => setStatement(data))
-            .catch(error => console.log(error));
+            // .catch(error => console.log(error));
     }, []);
 
     return (
